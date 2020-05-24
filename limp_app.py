334cd00d04f0a304c3a54b4fba3e7b92744e83c8
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-05-24 14:33:00 +0400

    Updated Config Structure

--- limp_app.py
+++ limp_app.py
@@ -1,4 +1,5 @@
 from limp.classes import APP_CONFIG, PACKAGE_CONFIG, ATTR
+from limp.enums import LIMP_VALUES
 
 config = APP_CONFIG(
 	name='limp_sample_app',
@@ -35,25 +36,53 @@ config = APP_CONFIG(
 	locales=['ar_AE', 'en_AE'],
 	# Define app default locale, which is the required value for LOCALE Attr Type
 	locale='ar_AE',
-	# Define user attrs, these are the app-specific attrs every User doc should have. You need to at least define one attr which should be later defined in 'user_auth_attrs'
+	# Define user attrs, these are unique attributes per user that can be used to authenticate the user
 	user_attrs={
 		'email':ATTR.EMAIL(),
-		# '...': ATTR....()
+		# 'username': ATTR.STR(),
+		# 'phone': ATTR.PHONE(),
 	},
-	# Define which attrs of 'user_attrs' should be used to use for 'Session.auth' calls. This means, the attrs defined in this list are unique and should have a corresponding hash
-	user_auth_attrs=['email'],
+	# Define user_settings, these are user-specific settings that are created with Setting module, in order to give access to users to these settings without breaching User module security model
+	user_settings={
+		# If you define a user_settings item with type: user, it means user has ability to change its value using setting/update call
+		# If you define a user_settings item with default: LIMP_VALUES.NONE_VALUE, it means this settings attr requires a value at time of creating user. Notice that we had to supply a value for ADMIN doc to avoid breaking LIMP app launch
+		'age': {
+			'type': 'user',
+			'val_type': ATTR.INT(ranges=[[18, 121]]),
+			'default': LIMP_VALUES.NONE_VALUE,
+		},
+		# If you would like to have a default value so that if user/create call doesn't fail for not specifying a value, you can set a matching default value
+		'bio': {
+			'type': 'user',
+			'val_type': ATTR.LOCALE(),
+			'default': {'ar_AE': None},
+		},
+		# If you define a user_settings item with type: user_sys, it means this settings attr is available for the user to read but not update
+		# For, this type settings attr, a default value HAS TO BE provided and it would be used, where any supplied value in user/create doc would be ignored for this settings attr
+		'points': {
+			'type': 'user_sys',
+			'val_type': ATTR.INT(),
+			'default': 0,
+		},
+	},
+	# Specify which of user_settings to be appended to User doc at time of reading it. This means attrs in the list would be present for all docs in user/read call results. Notice, 'age' is missing--To get User 'age' settings attr, setting/read call can be used
+	user_doc_settings=['bio', 'points'],
 	# Set ADMIN doc values. This is the default ADMIN doc value that will be created to allow you to authenticate and manage the data
 	admin_doc={
 		'email':'admin@app.limp.masaar.com',
 		# '...':'...'
+		'age': 120,
+		'points': 10,
 	},
 	# Define ADMIN password that will be used to generate the hashes
 	admin_password='S0mE_V3Ry_STR0nG_P@SsW0rD',
 	# Define ANON token. This token is used as salt across LIMP framework so it is very good idea to change it from default token
-	# anon_token= '__ANON_TOKEN_f00000000000000000000012',
-	# Define default privileges any authenticated user is having
-	# default_privileges={
-	# 	'module_name': ['privilege1', 'privilege2', ..., 'privilege0'],
-	# }
+	anon_token= '__ANON_TOKEN_f00000000000000000000012',
+	# Define default privileges any authenticated user is having. Usually it's suggested all users have file: create, setting: update, privileges as they are basic access requirements to any app, however, if you create proxy modules where you would like to have more control over these privileges you can always drop the default privileges
+	default_privileges={
+		'file': ['create'],
+		'setting': ['update'],
+		# 'module_name': ['privilege1', 'privilege2', ..., 'privilege0'],
+	}
 	# Learn more about Config Attrs on LIMP Docs.
 )
